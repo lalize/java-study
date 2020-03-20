65e1d2a317c8c2bf14bc19ffdedf949db7067582
lalize <printf@kakao.com>
2020-03-20 11:28:59 +0900

    refactor: sumAll 함수형으로 수정

--- src/main/java/techcourse/fp/mission/Calculator.java
+++ src/main/java/techcourse/fp/mission/Calculator.java
@@ -3,29 +3,30 @@ package techcourse.fp.mission;
 import java.util.List;
 
 public class Calculator {
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
-    }
+	public static int sumAll(List<Integer> numbers) {
+		return sumAll(numbers, number -> true);
+	}
 
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
-    }
+	public static int sumAllEven(List<Integer> numbers) {
+		return sumAll(numbers, number -> number % 2 == 0);
+	}
 
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
+	public static int sumAllOverThree(List<Integer> numbers) {
+		return sumAll(numbers, number -> number > 3);
+	}
 
-        //TODO: List에 담긴 값 중 3보다 큰 수만을 더해야 한다.
+	public static int sumAll(List<Integer> numbers, Conditional conditional) {
+		int total = 0;
+		for (int number : numbers) {
+			total = getTotal(conditional, total, number);
+		}
+		return total;
+	}
 
-        return total;
-    }
+	private static int getTotal(Conditional conditional, int total, int number) {
+		if (conditional.test(number)) {
+			total += number;
+		}
+		return total;
+	}
 }
